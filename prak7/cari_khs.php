cbee76a85c3a6e67a5a89f2fcb246e0925ba8e65
Cahyadi <cahyaditkj626@gmail.com>
2021-12-08 22:36:56 +0800

    upload fpdf pertemuan 8
    menambahkan codingan pertemuan 8 pada folfer pertemuan 7 dengan nama lap_mhs.php. dan juga menambahkan library fpdf pada projek

--- prak7/cari_khs.php
+++ prak7/cari_khs.php
@@ -1,15 +1,20 @@
 <?php
+// import file koneksi
 include 'koneksi.php';
 ?>
 <h3>Form Pencarian DATA KHS Dengan PHP </h3>
+<!-- Form untuk melakukan pencarian -->
 <form action="" method="get">
   <label>Cari :</label>
   <input type="text" name="cari">
   <input type="submit" value="Cari">
 </form>
+<!-- End Form pencarian -->
+<!-- melakukan pengecekan jika user memasukan kata kunci -->
 <?php
 if (isset($_GET['cari'])) {
   $cari = $_GET['cari'];
+  // mencetak hasil dari kata kunci yang di inputkan user
   echo "<b>Hasil pencarian : " . $cari . "</b>";
 }
 ?>
@@ -17,10 +22,13 @@ if (isset($_GET['cari'])) {
   <tr>
     <th>No</th>
     <th>NIM</th>
-    <th>Kode MK</th>
+    <th>Nama Mhs</th>
+    <th>Kode Matkul</th>
+    <th>Nama Matkul</th>
     <th>Nilai</th>
   </tr>
   <?php
+  // menampilkan data yang dicari oleh user ssuai dengan kata kunci yang dicari yang ada dalam table khs
   if (isset($_GET['cari'])) {
     $cari = $_GET['cari'];
     $sql = "select * from khs where nim like '%" . $cari . "%'";
@@ -29,14 +37,18 @@ if (isset($_GET['cari'])) {
     $sql = "select * from khs";
     $tampil = mysqli_query($con, $sql);
   }
+  // perulangan yang dilakukan untuk menampilkan data nim, kodemk dan nilai
   $no = 1;
   while ($r = mysqli_fetch_array($tampil)) {
   ?>
     <tr>
       <td><?php echo $no++; ?></td>
       <td><?php echo $r['nim']; ?></td>
+      <td><?php echo $r['nama_mhs']; ?></td>
       <td><?php echo $r['kodemk']; ?></td>
+      <td><?php echo $r['nama_mk']; ?></td>
       <td><?php echo $r['nilai']; ?></td>
     </tr>
   <?php } ?>
+  <!-- end perulangan -->
 </table>
--- prak7/cari_mhs.php
+++ prak7/cari_mhs.php
@@ -1,15 +1,20 @@
 <?php
+// import file koneksi
 include 'koneksi.php';
 ?>
 <h3>Form Pencarian Dengan PHP MAHASISWA</h3>
+<!-- Form untuk melakukan pencarian -->
 <form action="" method="get">
   <label>Cari :</label>
   <input type="text" name="cari">
   <input type="submit" value="Cari">
 </form>
+<!-- End Form pencarian -->
+<!-- melakukan pengecekan jika user memasukan kata kunci -->
 <?php
 if (isset($_GET['cari'])) {
   $cari = $_GET['cari'];
+  // mencetak hasil dari kata kunci yang di inputkan user
   echo "<b>Hasil pencarian : " . $cari . "</b>";
 }
 ?>
@@ -19,6 +24,7 @@ if (isset($_GET['cari'])) {
     <th>Nama</th>
   </tr>
   <?php
+  // menampilkan data yang dicari oleh user ssuai dengan kata kunci yang dicari yang ada dalam table mahasiswa
   if (isset($_GET['cari'])) {
     $cari = $_GET['cari'];
     $sql = "select * from mahasiswa where nama like'%" . $cari . "%'";
@@ -27,13 +33,15 @@ if (isset($_GET['cari'])) {
     $sql = "select * from mahasiswa";
     $tampil = mysqli_query($con, $sql);
   }
+
+  // perulangan yang dilakukan untuk menampilkan data nama mahasiswa
   $no = 1;
   while ($r = mysqli_fetch_array($tampil)) {
-    // while ($r = mysqli_result_array($tampil)) {
   ?>
     <tr>
       <td><?php echo $no++; ?></td>
       <td><?php echo $r['nama']; ?></td>
     </tr>
   <?php } ?>
+  <!-- akhir perulangan -->
 </table>
