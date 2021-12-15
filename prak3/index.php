76578194c2f999a901540288c250350b08d7c2d5
Cahyadi <cahyaditkj626@gmail.com>
2021-12-15 23:12:28 +0800

    tambahkan file prak 9

--- prak3/index.php
+++ prak3/index.php
@@ -1,7 +1,7 @@
 <?php
-// Create database connection using config file
+// Melakukan koneksi ke database
 include_once("koneksi.php");
-// Fetch all users data from database
+// mengambil data dari table mahasiswa
 $result = mysqli_query($con, "SELECT * FROM mahasiswa ");
 ?>
 <html>
@@ -9,7 +9,7 @@ $result = mysqli_query($con, "SELECT * FROM mahasiswa ");
 <head>
   <title>Halaman Utama</title>
 
-  <!-- Latest compiled and minified CSS -->
+  <!-- link ke boostrap  -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 </head>
@@ -17,18 +17,24 @@ $result = mysqli_query($con, "SELECT * FROM mahasiswa ");
 <body>
   <br>
   <h1>Data Mahasiswa</h1>
-  <a href="tambah.php" class="btn btn-primary">Tambah Data Baru</a><br /><br />
+  <!-- button tambah data -->
+  <a href="tambah.php" class="btn btn-primary">Tambah Data Baru</a>
+  <!-- button cekak data laporan mahasiswa -->
+  <a href="lap_mhs.php" class="btn btn-success">
+    Cetak data Mahasiswa
+  </a>
+  <br> <br>
   <table width='80%' class="table">
     <tr>
       <th>Nim</th>
       <th>Nama</th>
       <th>Gender</th>
       <th>Alamat</th>
-      <th>tgl lahir</th>
+      <th>Tanggal lahir</th>
       <th>Update</th>
     </tr>
     <?php
-
+    // perulangan untuk mencetak data dari table 
     while ($user_data = mysqli_fetch_array($result)) {
       echo "<tr>";
       echo "<td>" . $user_data['nim'] . "</td>";
@@ -39,7 +45,6 @@ $result = mysqli_query($con, "SELECT * FROM mahasiswa ");
       echo "<td> <a class='btn btn-primary' href='edit.php?nim=$user_data[nim]'>Edit </a> | <a class='btn btn-danger' href='delete.php?nim=$user_data[nim]'>Delete</a></td></tr>";
     }
     ?>
-
   </table>
 </body>
 
