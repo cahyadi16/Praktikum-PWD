65287bd504f7c51832a35267daa88cf93debcbca
Cahyadi <cahyaditkj626@gmail.com>
2021-12-22 22:55:34 +0800

    pertemuan 10

--- prak9/mhs_json.php
+++ prak9/mhs_json.php
@@ -1,12 +1,17 @@
 <?php
+// memanggil file koneksi ke database
 include "koneksi.php";
+// deklarasi variable sql yang digunakan mengambil data mahasiswa berdasarkan nim
 $sql = "select * from mahasiswa order by nim";
 $tampil = mysqli_query($con, $sql);
+// melakukan pencarian data jika datanya ada
 if (mysqli_num_rows($tampil) > 0) {
   $no = 1;
+  // deklarasi respons data kedalam objek assosiativ array
   $response = array();
   $response["data"] = array();
   while ($r = mysqli_fetch_array($tampil)) {
+    // menampilkan data sesuai dengan isi yang ada di table
     $h['nim'] = $r['nim'];
     $h['nama'] = $r['nama'];
     $h['jkel'] = $r['jkel'];
@@ -14,7 +19,9 @@ if (mysqli_num_rows($tampil) > 0) {
     $h['tgllhr'] = $r['tgllhr'];
     array_push($response["data"], $h);
   }
+  // menampilkan data respons kedalam bentuk json
   echo json_encode($response);
+  // jika tidak ada data makan cetak pesan tidak ada data
 } else {
   $response["message"] = "tidak ada data";
   echo json_encode($response);
--- prak9/mhs_xml.php
+++ prak9/mhs_xml.php
@@ -1,11 +1,18 @@
 <?php
+// memanggil file koneksi
 include "koneksi.php";
+// melakukan panggilan untuk menampilkan data kedalam bentuk xml
 header('Content-Type: text/xml');
+// query melakukan pengambilan data dari table mahasiswa
 $query = "SELECT * FROM mahasiswa";
 $hasil = mysqli_query($con, $query);
+// definisi jumlah field yang ada table
 $jumField = mysqli_num_fields($hasil);
+// mencetak xml dengan versi 1.0
 echo "<?xml version='1.0'?>";
+
 echo "<data>";
+// melakukan perulangan untuk mencetak data dari table mahasiswa 
 while ($data = mysqli_fetch_array($hasil)) {
   echo "<mahasiswa>";
   echo "<nim>", $data['nim'], "</nim>";
