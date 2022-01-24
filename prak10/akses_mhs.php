7a437fbfab13d2f898e7ff9ce700a7291150bcf4
Cahyadi <cahyaditkj626@gmail.com>
2022-01-24 08:04:29 +0800

    perbaikan code

--- prak10/akses_mhs.php
+++ prak10/akses_mhs.php
@@ -1,17 +1,22 @@
 <?php
 // setting url
 $url = "http://localhost:8080/Praktikum-PWD/prak10/getdatamhs.php";
-
+// menginisialisasi curl url yang sudah di tampung tadi 
 $client = curl_init($url);
+// return dari web service menjadi sebuah string
 curl_setopt($client, CURLOPT_RETURNTRANSFER, 1);
+// malkukan eksekusi dari web service
 $response = curl_exec($client);
+// mengubah format data string menjadi format json 
+
 $result = json_decode($response);
+// melakukan perulangan data dari data yang kita buat 
 foreach ($result as $r) {
-  echo "<p>";
-  echo "NIM : " . $r->nim . "<br />";
-  echo "Nama : " . $r->nama . "<br />";
-  echo "jenis kel : " . $r->jkel . "<br />";
-  echo "Alamat : " . $r->alamat . "<br />";
-  echo "Tgl Lahir : " . $r->tgllhr . "<br />";
-  echo "</p>";
+  echo "<p>"; //pombuka tag paragraf
+  echo "NIM : " . $r->nim . "<br />"; //mencetak nim
+  echo "Nama : " . $r->nama . "<br />"; //mencetak nama
+  echo "jenis kel : " . $r->jkel . "<br />"; //mencetak jenis kelamin
+  echo "Alamat : " . $r->alamat . "<br />"; //mencetak alamat
+  echo "Tgl Lahir : " . $r->tgllhr . "<br />"; //mencetak tgl lahir
+  echo "</p>"; // penutup tag paragraf
 }
--- prak10/getdatamhs.php
+++ prak10/getdatamhs.php
@@ -1,10 +1,20 @@
 <?php
+// berfungi untuk melakukan pemanggilan ke file koneksi.php
 require_once "../prak9/koneksi.php";
-$sql = "select * from mahasiswa";
+// inisialisasi nim yang digunakan sebgai kata kunci
+$nim = '1800018070';
+// query yang dugunakan untuk mengambil semua data berdasarkan nim  dari table mahasiswa dan menampungnya kedalam variable sql
+$sql = "select * from mahasiswa where nim = '$nim'";
+// perintah query ke database mysql
 $query = mysqli_query($con, $sql);
+// perulangan yang akan menangkap data dari hasil perintah query dan mengembalikannya kedalam array assosiatif
 while ($row = mysqli_fetch_assoc($query)) {
+  // membuat variable data dalam bentuk array uang akan menampung data dari table mahasiswa
   $data[] = $row;
 }
+// memanggil content type berupa file json
 header('content-type: application/json');
+// fungsi yang akan merubah data array tadi kedalam format json
 echo json_encode($data);
+// memutus koneksi ke database
 mysqli_close($con);
